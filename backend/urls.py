361272be0ed486d976064127630bce06f6a33c7b
doni <donihamster88@gmail.com>
2025-11-03 13:15:15 +0700

    ganti router ke login

--- backend/urls.py
+++ backend/urls.py
@@ -25,6 +25,6 @@ from adminapi.views import CustomTokenObtainPairView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('adminapi.urls')),
-    path('api/token', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
